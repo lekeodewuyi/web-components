25a1984e062a3195435b7e5dfb8038cc65b32df2
Oluwaleke Odewuyi <lekeodewuyi@gmail.com>
2020-09-28 04:40:11 -0400

    feat: defines tooltip innerHTML template

--- src/scripts/tooltip.js
+++ src/scripts/tooltip.js
@@ -1,3 +1,78 @@
+const template = document.createElement('template');
+
+template.innerHTML += `
+    
+    <style>
+        .tooltip-container {
+            display: inline-block;
+            position: relative;
+            z-index: 2;
+        }
+
+        .tooltip-popup-message {
+            position: absolute;
+            bottom: 125%;
+            z-index: 10;
+            width: 150px;
+            background: white;
+            box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.1);
+            font-size: 0.8em;
+            line-height: 1.2;
+            border-radius: 0.5em;
+            padding: 1em;
+            transform: scale(0);
+            transform-origin: bottom left;
+            transition: 0.3s;
+        }
+
+        .tooltip-icon:hover ~ .tooltip-popup-message{
+            transform: scale(1);
+            transition: transform 0.5s cubic-bezier(0.8, 0, 0.1, 2);
+        }
+
+        .tooltip-text {
+            display: inline-block;
+            font-size: 1em;
+        }
+
+        .tooltip-text:hover ~ .tooltip-popup-message {
+            transform: scale(1);
+            transition: transform 0.5s cubic-bezier(0.8, 0, 0.1, 2);
+        }
+
+        .tooltip-svg {
+            font-size: 1em;
+            width: 1em;
+            display: inline-block;
+            fill: inherit;
+            position: relative;
+            top: 2px;
+        }
+
+        .tooltip-svg:hover ~ .tooltip-popup-message {
+            transform: scale(1);
+            transition: transform 0.5s cubic-bezier(0.8, 0, 0.1, 2);
+        }
+    </style>
+
+
+    <div class="tooltip-container">
+
+        <span class="tooltip-text">
+            <slot name="text" />
+        </span>
+
+        <span class="tooltip-svg">
+            <slot name="svg" />
+        </span>
+
+        <div class="tooltip-popup-message">
+            <slot name="popup-message" />
+        </div>
+
+    </div>
+`;
+
 class Tooltip extends HTMLElement {
     constructor() {
         super();
